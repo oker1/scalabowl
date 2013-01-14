f5fa7d1204b18aeec914dad65689cab9eac38485
Zsolt Takács <zsolt@takacs.cc>
2013-01-14 22:39:10 +0100

    reduce test duplication

--- src/test/scala/bowling/BowlingTest.scala
+++ src/test/scala/bowling/BowlingTest.scala
@@ -7,63 +7,59 @@ import matchers.ShouldMatchers
  * @author oker <zsolt@takacs.cc>
  */
 
-class BowlingTest extends FlatSpec with ShouldMatchers {
+class BowlingTest extends FlatSpec with ShouldMatchers with BeforeAndAfterEach {
+  var game: Bowling = _
+
   "A bowling game" should "score all misses zero" in {
-    val game = new Bowling
-    game.score(List(new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0))) should equal (0)
+    game.score(List(
+      new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0),
+      new Frame(0, 0),new Frame(0, 0), new Frame(0, 0), new Frame(0, 0), new Frame(0, 0)
+    )) should equal (0)
   }
   "A bowling game" should "score a 5 and a 1 6" in {
-    val game = new Bowling
     game.score(List(new Frame(5, 1))) should equal (6)
   }
   "A bowling game" should "score a 5 and a spare 10" in {
-    val game = new Bowling
     game.score(List(new Spare(5))) should equal (10)
   }
   "A bowling game" should "score a 5 and a miss 5" in {
-    val game = new Bowling
     game.score(List(new Frame(5, "-"))) should equal (5)
   }
   "A bowling game" should "score a strike 10" in {
-    val game = new Bowling
     game.score(List(new Strike)) should equal (10)
   }
   "A bowling game" should "score two misses 0" in {
-    val game = new Bowling
     game.score(List(new Miss())) should equal (0)
   }
   "A bowling game" should "score a miss and a spare 10" in {
-    val game = new Bowling
     game.score(List(new Spare("-"))) should equal (10)
   }
   "A bowling game" should "score a miss and a 1 1" in {
-    val game = new Bowling
     game.score(List(new Frame(0, 1))) should equal (1)
   }
   "A bowling game" should "score a 5 and a spare and a 3 16" in {
-    val game = new Bowling
     game.score(List(new Spare(5), new Frame(3, 0))) should equal (16)
   }
   "A bowling game" should "score a strike and a 5 and a miss 20" in {
-    val game = new Bowling
     game.score(List(new Strike(), new Frame(5, 0))) should equal (20)
   }
   "A bowling game" should "score three strikes 60" in {
-    val game = new Bowling
     game.score(List(new Strike(), new Strike(), new Strike())) should equal (60)
   }
   "A bowling game" should "score a perfect game 300" in {
-    val game = new Bowling
     game.score(List(
       new Strike(), new Strike(), new Strike(), new Strike(), new Strike(),
       new Strike(), new Strike(), new Strike(), new Strike(), new Strike(), new BonusStrike(), new BonusStrike()
     )) should equal (300)
   }
   "A bowling game" should "score an all 5 spare game 150" in {
-    val game = new Bowling
     game.score(List(
       new Spare(5), new Spare(5), new Spare(5), new Spare(5), new Spare(5),
       new Spare(5), new Spare(5), new Spare(5), new Spare(5), new Spare(5), new Bonus(5)
     )) should equal (150)
+  }
+
+  override def beforeEach() {
+    game = new Bowling
   }
 }
