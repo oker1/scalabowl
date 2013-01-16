3631eb5b4dac444e209a6800dc96bc737f7c30e2
Zsolt Takács <zsolt@takacs.cc>
2013-01-16 22:20:29 +0100

    simplify scoring

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -6,37 +6,28 @@ package bowling
 
 class Bowling {
   def score(game: List[Frame]): Int = {
-    if (game.isEmpty) {
-      0
-    } else {
-      val current = game.head
-      val rest = game.drop(1)
-
-      val additionalScore =
-        game.take(3) match {
-          case Spare(_) :: tail => tail match {
-            case Spare(firstScore) :: _ => firstScore
-            case TwoRolls(x, y) :: _ => x
-            case List(Frame(score), _*)      => score
-            case Nil           => 0
-          }
-          case Strike() :: tail => tail match {
-            case TwoRolls(x, y) :: _ => x + y
-            case Spare(_) :: _ => 10
-            case List(Frame(f1), Frame(f2)) => f1 + f2
-            case List(Frame(score))         => score
-            case Nil           => 0
-          }
-          case Frame(_) :: _ => 0
-          case Nil           => 0
+    val additionalScore =
+      game.take(3) match {
+        case Spare(_) :: tail => tail match {
+          case Spare(firstScore) :: _ => firstScore
+          case TwoRolls(x, y) :: _ => x
+          case List(Frame(score), _*) => score
+          case Nil => 0
         }
-
-      val currentScore = current match {
-        case Bonus(_) => 0
-        case Frame(x) => x
+        case Strike() :: tail => tail match {
+          case TwoRolls(x, y) :: _ => x + y
+          case Spare(_) :: _ => 10
+          case List(Frame(f1), Frame(f2)) => f1 + f2
+          case List(Frame(score)) => score
+          case Nil => 0
+        }
+        case x => 0
       }
 
-      currentScore + additionalScore + score(rest)
+    game match {
+      case Bonus(_) :: _ => additionalScore + score(game.drop(1))
+      case Frame(x) :: _ => x + additionalScore + score(game.drop(1))
+      case Nil           => 0
     }
   }
 }
